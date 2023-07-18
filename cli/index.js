f779d6629ca46f06ad4f1eb91d09b4df8fe1e5c2
Ed Preston <epreston@prestonsoft.com>
2023-07-19 04:09:12 +1000

    Add CLI Test Rig

---
+++ cli/index.js
@@ -0,0 +1,53 @@
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
+
+async function main() {
+  console.log(colors.bold.green('ChatGPT ChitChat - CTRL+C to exit'));
+
+  const chatHistory = [];
+  let doLoop = true;
+
+  process.on('SIGINT', () => {
+    doLoop = false;
+  });
+
+  while (doLoop) {
+    const userInput = readlineSync.question(colors.yellow('You: '));
+
+    try {
+      // Construct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      // Add latest user input
+      messages.push({ role: 'user', content: userInput });
+
+      // Call the API with user input & history
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+      });
+
+      // Get completion text/content
+      const completionText = completion.data.choices[0].message.content;
+
+      if (userInput.toLowerCase() === 'exit') {
+        console.log(colors.grey('Assistant: ') + completionText);
+        return;
+      }
+
+      console.log(colors.grey('Assistant: ') + completionText);
+
+      // Update history with user input and assistant response
+      chatHistory.push(['user', userInput]);
+      chatHistory.push(['assistant', completionText]);
+    } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
+}
+
+main();
