7345c661dbf3441c79e15b8805daa68260c5a253
Ed Preston <epreston@prestonsoft.com>
2023-08-12 03:33:58 +1000

    Update global.d.ts
    cleanup

--- global.d.ts
+++ global.d.ts
@@ -10,22 +10,3 @@ declare namespace jest {
     toHaveBeenWarnedTimes(n: number): R
   }
 }
-
-
-// additional import types not included in vite/client
-// declare module '*.m4a' {
-//   const src: string
-//   export default src
-// }
-
-// declare module '*.glb' {
-//   const src: string
-//   export default src
-// }
-
-// declare module '*.gltf' {
-//   const src: string
-//   export default src
-// }
-
-// declare module '*.vue' {}
