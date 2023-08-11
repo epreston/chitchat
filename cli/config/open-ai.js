3bbc605b75e9d62072d7726e00c50f133b2deacb
Ed Preston <epreston@prestonsoft.com>
2023-08-12 03:29:06 +1000

    Update open-ai.js
    minor formatting

--- cli/config/open-ai.js
+++ cli/config/open-ai.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
